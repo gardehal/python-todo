333ac8a8293d9eb9e5109bc9619eabbdb42a5a79
grdall <alex.thogar@gmail.com>
2019-11-01 23:19:15 +0100

    tick and delete tasks

--- todo.py
+++ todo.py
@@ -10,8 +10,7 @@ class Main:
         taskListPath = "tasklists"
         resetChecked = None
 
-        listTasks = False
-        addTask = False
+        # TODO: groom/trigger reset
 
         argC = len(sys.argv)
         argIndex = 1
@@ -20,16 +19,17 @@ class Main:
 
             # List curret todo list ()
             if(arg == "-list" or arg == "-ls"):
-                listTasks = True
+                Main.printList(taskList, taskListPath)
+                argIndex += 1
+                continue
 
             # Add a task
             elif(arg == "-add" or arg == "-a"):
                 if(argC - argIndex < 2):
-                    print("Too few arguments to save task, need at least 1: task (string), task list (string)(optional), resetChecked (enum)(optional)")
+                    print("Too few arguments to save task, need at least 1: task (string), tasklist (string)(optional), resetChecked (enum)(optional)")
                     quit()
 
                 task = sys.argv[argIndex + 1]
-                addTask = True
 
                 # reset args?
                 # hour reset after completed easiest? h 2 -> write Z or T + datetime first in line, when next list is called, check timestamp, compare to reset time in args, 
@@ -47,21 +47,61 @@ class Main:
                 if(argC > argIndex + 3 and sys.argv[argIndex + 3][0] != "-"):
                     resetChecked = sys.argv[argIndex + 3]
                     optionalArgIndex += 1
+                # if(argC > argIndex + 4 and sys.argv[argIndex + 4][0] != "-"):
+                #     resetChecked = sys.argv[argIndex + 3]
+                #     optionalArgIndex += 1
+                    
+                saveRes = Main.addTask(task, taskList, taskListPath)
+                print("Add task " + ("was successful." if saveRes else "failed."))
 
                 argIndex += 2 + optionalArgIndex
                 continue
 
             # Delete a task
             elif(arg == "-delete" or arg == "-d"):
-                print("d")
+                if(argC - argIndex < 2):
+                    print("Too few arguments to save task, need at least 1: tasknumber (int), tasklist (string)(optional), taskListPath (string)(optional)")
+                    quit()
+                
+                taskNumber = int(sys.argv[argIndex + 1]) - 1
 
-            # Check off a task (mark as done)
+                # Optional arguments, tasklist (file) and path
+                optionalArgIndex = 0
+                if(argC > argIndex + 2 and sys.argv[argIndex + 2][0] != "-"):
+                    taskList = sys.argv[argIndex + 2]
+                    optionalArgIndex += 1
+                if(argC > argIndex + 3 and sys.argv[argIndex + 3][0] != "-"):
+                    resetChecked = sys.argv[argIndex + 3]
+                    optionalArgIndex += 1
+
+                delRes = Main.editTask(taskNumber, taskList, taskListPath, "delete", "w")
+                print(("Successfully" if delRes else "Failed to") + " delete task.")
+
+                argIndex += 2 + optionalArgIndex
+                continue
+
+            # Toggle check/tick off a task (mark as done)
             elif(arg == "-check" or arg == "-c" or arg == "-x" or arg == "-tick" or arg == "-t"):
-                print("c")
+                if(argC - argIndex < 2):
+                    print("Too few arguments to save task, need at least 1: tasknumber (int), tasklist (string)(optional), taskListPath (string)(optional)")
+                    quit()
+                
+                taskNumber = int(sys.argv[argIndex + 1]) - 1
 
-            # Uncheck a task (unmark as done)
-            elif(arg == "-uncheck" or arg == "-uc"):
-                print("uc")
+                # Optional arguments, tasklist (file) and path
+                optionalArgIndex = 0
+                if(argC > argIndex + 2 and sys.argv[argIndex + 2][0] != "-"):
+                    taskList = sys.argv[argIndex + 2]
+                    optionalArgIndex += 1
+                if(argC > argIndex + 3 and sys.argv[argIndex + 3][0] != "-"):
+                    resetChecked = sys.argv[argIndex + 3]
+                    optionalArgIndex += 1
+
+                tickRes = Main.editTask(taskNumber, taskList, taskListPath, "tick", "w")
+                print(("Successfully" if tickRes else "Failed to") + " tick task.")
+
+                argIndex += 2 + optionalArgIndex
+                continue
 
             # Change list of tasks
             elif(arg == "-changeList" or arg == "-cl"):
@@ -82,15 +122,6 @@ class Main:
                 quit()
 
             argIndex += 1
-
-        # TODO: groom/trigger reset
-        
-        if(addTask):
-            saveRes = Main.addTask(task, taskList, taskListPath)
-            print("Add task " + ("was successful." if saveRes else "failed."))
-
-        if(listTasks):
-            Main.printList(taskList, taskListPath)
 
         print("\nend")
 
@@ -116,7 +147,6 @@ class Main:
         """
 
         fullPath = Main.getFullFilePath(taskListPath, taskList)
-        print(fullPath)
 
         fileExists = False
         try:
@@ -130,7 +160,7 @@ class Main:
         if(not os.path.exists(taskListDirectory)):
             os.mkdir(taskListDirectory)
 
-        taskLine = ("\n" if fileExists else "") + "0" + task
+        taskLine = ("\n" if fileExists else "") + "0 " + task
 
         try:
             with open(fullPath, "a") as file:
@@ -163,48 +193,60 @@ class Main:
 
         return res
 
-    def removeTask(task, taskList, taskListPath):
-        """
-        A method for removing a task from a tasklist. \n
-        string task
-        string taskList \n
-        string taskListPath
-        """
-
-        print("--- removeTask ---")
-
-    def toggleDone(taskNumber, taskList, taskListPath):
-        """
-        A method for removing a task from a tasklist. \n
-        int taskNumber
-        string taskList \n
-        string taskListPath
-        """
-
-        tasks = Main.loadTaskList(taskList, taskListPath)
-        tasks[taskNumber - 1]
-
-        print("--- toggleDone ---")
-
 # idea: a generic editing method can let me add (append), delete, change check status, set new reset time etc
-    def editTaskList(taskList, taskListPath, filePermissions, taskNumber):
+    def editTask(taskNumber, taskList, taskListPath, action, filePermissions = "rw+"):
         """
         A method editing a tasklist. \n
+        int taskNumber \n
         string taskList \n
         string taskListPath \n
-        string filePermissions \n
-        array seekArgs
+        string action \n
+        string filePermissions
         """
+
         fullPath = Main.getFullFilePath(taskListPath, taskList)
-
+        fileArray = []
+        # Read
         try:
-            with open(fullPath, filePermissions) as file:
-                for line in file:
-                    res.append(line)
+            with open(fullPath, "r") as readFile:
+                for line in readFile:
+                    fileArray.append(line)
 
-            file.close() 
+            readFile.close()
+        except:
+            print("Error reading file.")
+            return False
+
+        if(len(fileArray) == 0):
+            print("The task list " + taskList + " is empty.")
+            quit()
+        if(taskNumber < 0 or taskNumber > len(fileArray)):
+            print("Invalid task number, for task list " + taskList + " minimum is 1 and maximum is " + str(len(fileArray)))
+            quit()
+
+
+        taskNumber = int(taskNumber)
+        # Edit array
+        if(action == "tick"):
+            fileArray[taskNumber] = ("1" if fileArray[taskNumber][0] == "0" else "0") + fileArray[taskNumber][1:]
+        elif(action == "delete"):
+            fileArray.pop(taskNumber)
+        else:
+            print("Action not recognized, must be \"tick\", \"delete\"")
+            quit()
+
+        # Write back to array
+        try:
+            with open(fullPath, filePermissions) as writeFile:
+                index = 0
+                while index < len(fileArray):
+                    writeFile.write(fileArray[index])
+                    index += 1
+
+            writeFile.close() 
             return True
         except:
+            print("Error writing to file.")
             return False
 
     def setList(taskList, taskListPath):
